361a68b39366b2ff4ba6c3a7841d5587828b1b16
chinnnawat <chin3896@gmail.com>
2023-12-29 16:35:34 +0700

    Println

--- demo-backend/main.go
+++ demo-backend/main.go
@@ -1,9 +1,33 @@
 package main
 
-import(
+import (
 	"fmt"
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
 )
 
-func main()  {
-	fmt.Println("Hello")
+func main() {
+	// fmt.Println("Hello")
+	initViper()
+	title := viper.Get("Dashboard.title")
+	fmt.Println(title)
+}
+
+func initViper() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	viper.SetEnvPrefix("demo")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		// fmt.Errorf = หากมี error ให้ show "unable to initialize viper: %w"
+		// %w = ใช้ แสดง error หากเกิด error ขึ้น
+		panic(fmt.Errorf("unable to initialize viper: %w", err))
+	}
+	log.Println("viper config initialized")
 }
